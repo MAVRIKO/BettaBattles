5da2a9b07db86f730e42fde861f115db0a961880
MAVRIKO <36166054+MAVRIKO@users.noreply.github.com>
2020-01-13 21:44:44 -0500

    relocated files

--- BettaBattles/Assets/Scripts/Player.cs
+++ BettaBattles/Assets/Scripts/Player.cs
@@ -49,6 +49,7 @@ public class Player : MonoBehaviour
         previouseAP = new Vector2(Mathf.Lerp(previousInput.x, movement.x, attackPointResponciveness * Time.deltaTime), Mathf.Lerp(previousInput.y, movement.y, attackPointResponciveness * Time.deltaTime));
         attackPoint = new Vector2(transform.position.x, transform.position.y) + previouseAP * attackDistance;
         previousInput = previouseAP;
+        transform.LookAt(attackPoint);
 
         Move();
     }
